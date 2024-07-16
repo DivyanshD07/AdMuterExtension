4fd41c226fc0a280b4ec058fac3da4f734eab5d3
Divyansh Dhyani <devdhyani34@gmail.com>
2024-07-16 22:33:54 +0530

    update: skip ads

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 let isMuted = false;
+let adCheckTimeout = null;
 
 function muteVideo(video) {
     if(!video.muted) {
@@ -14,15 +15,38 @@ function unmuteVideo(video) {
     }
 }
 
-function checkForAds() {
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function checkAndSkipAd() {
+    await delay(6000);
+    const skipButton = document.querySelector('.ytp-ad-skip-button');
+    
+    if(skipButton) {
+        skipButton.click();
+        return true;
+    }
+    return false;
+}
+
+async function handleAds() {
     const videos = document.querySelectorAll('video');
-    videos.forEach(video => {
-        if (video.duration <= 30) {
+    for (const video of videos) {
+        if (video.duration <= 40) {
             muteVideo(video);
+            const isSkippable = await checkAndSkipAd();
+            if(!isSkippable) {
+                const adDuration = video.duration - 1;
+                clearTimeout(adCheckTimeout);
+                adCheckTimeout = setTimeout(() => {
+                    handleAds();
+                }, adDuration * 1000);
+            }            
         } else{
             unmuteVideo(video);
         }
-    });
+    };
 }
 
-setInterval(checkForAds, 1000); // need to improve this
+setInterval(() => { handleAds(); }, 1000); // need to improve this
